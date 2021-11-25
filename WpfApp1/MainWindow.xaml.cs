dfd6efd445258f650743ed654428a411a7baf8db
Максим Манин <maks.manin.04@gmail.com>
2021-11-25 19:34:34 +1000

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = new TimeSpan(0, 0, 3);
+            timer.Tick += Timer_Tick;
+        }
+
+        private DispatcherTimer timer = new DispatcherTimer();
+        private int index = 0;
+
+        private void bStartJourney_Click(object sender, RoutedEventArgs e)
+        {
+            bStartJourney.Visibility = Visibility.Collapsed;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            textCatJourney.Text = messages[index++];
+        }
+
+        private string[] messages =
+        {
+            "Лёха Пожиратель Миров съел целый мир",
+            "Лёха Пожиратель Миров растворяет мир в глубинах своего желудка",
+            "Лёха Пожиратель Миров заснул великим сном",
+            "Вселенные исчезают пока Лёха Пожиратель Миров видит великие сны",
+            "Лёха Пожиратель Миров вот-вот восстанет ото сна, и отправится искать очередной мир",
+            "Лёха Пожиратель Миров в поисках своей новой жертвы",
+            "Лёха Пожиратель Миров обратил свой взор на чей-то уютный мирок",
+            "Лёха Пожиратель Миров съел ещё один мир",
+        };
+    }
+}
